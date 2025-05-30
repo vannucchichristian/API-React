503f23ea81fecf50e31583cf4c7b416002ed8575
brunganiccolo <brunga.niccolo@studenti-ittfedifermi.edu.it>
2025-05-30 10:38:58 +0200

    Aggiunta pull-to-refresh per ricaricare i dati

--- App.js
+++ App.js
@@ -42,6 +42,8 @@ export default function App() {
                             />
                         </View>
                     )}
+                    refreshing={loading}
+                    onRefresh={fetchData}
                 />
             )}
         </View>
